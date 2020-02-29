c14b2e1bdb2f52a96fc9db2bb57504bfaea3a026
Daniil Sherkozhukhov <d.a.sherkozhukhov1995@gmail.com>
2020-03-01 02:15:29 +0300

    block__project

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ $('.icon-menu').click(function(event) {
 	$('body').toggleClass('lock');
 });
 
+
 function ibg(){
 	$.each($('.ibg'), function(index, val) {
 		if($(this).find('img').length>0){
@@ -15,10 +16,34 @@ function ibg(){
 }
 ibg();
 
+
 $(document).ready(function() {
 	$('.header__burger').click(function(event) {
 		$('.header__burger,.header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
 });
+
+
+document.addEventListener('DOMContentLoaded', function(e){
+    'use strict';
+    var list = document.querySelectorAll('#project__toggle a');
+    list = Array.prototype.slice.call(list, 0); // convert nodeList to Array
+    list.forEach(function(el, i, ar) {
+        el.addEventListener('click', function(event){
+            e.preventDefault();
+            var tab = document.querySelector(el.getAttribute('href'));
+
+            // remove "act" class
+            document.querySelector('#project__toggle .act')
+                .classList.remove('act');
+            document.querySelector('#project__content .act')
+                .classList.remove('act');
+
+            // set "act"
+            el.classList.add('act');
+            tab.classList.add('act');
+        })
+    })
+})
 
